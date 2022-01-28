96cc0263db1ee39e5e6d042a303599d4ef5c9dbe
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-01-28 12:03:31 +0530

    A mess

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -15,3 +15,20 @@ try {
 } catch (e) {
   alert("Error with code or your browser does not support Locale");
 }
+
+
+function toggleMenu() {
+  document.getElementById("navicron").classList.toggle("open");
+  document.getElementById("hamburburBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburburBtn");
+x.onclick = toggleMenu;
+
+
+
+const datefieldUK = document.querySelector("aside");
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
