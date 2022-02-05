62016efd8457f1d925bce3964447228c0bda06d8
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-02-05 05:38:17 +0530

    Last Update Fix

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -4,17 +4,10 @@ document.getElementById("currentyear").textContent = year;
 
 const date2 = document.querySelector("#currentdate");
 
-try {
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  };
-  date2.textContent = new Date().toLocaleDateString("en-UK", options);
-} catch (e) {
-  alert("Error with code or your browser does not support Locale");
-}
+
+document.querySelector(
+  ".last-updated"
+).textContent = `Last Updated: ${document.lastModified}`;
 
 const now = new Date();
 const datehead =document.getElementById("time-today");
