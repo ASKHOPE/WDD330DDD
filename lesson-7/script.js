ef30da9fb1547fdb3f64a7669f46b1ab2e539df7
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-02-18 02:37:15 +0530

    Almost finished

--- lesson-7/script.js
+++
@@ -1,9 +0,0 @@
-const date1 = new Date();
-const year = date1.getFullYear();
-document.getElementById("currentyear").textContent = year;
-
-const date2 = document.querySelector("#currentdate");
-
-document.querySelector(
-  ".last-updated"
-).textContent = `Last Updated: ${document.lastModified}`;
