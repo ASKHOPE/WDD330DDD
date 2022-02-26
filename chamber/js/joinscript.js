ef31d650d970a7c2d591779d095a278c48b6eb7f
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-02-27 01:32:30 +0530

    Fixed CSS and Hamburger button

--- chamber/js/joinscript.js
+++ chamber/js/joinscript.js
@@ -3,17 +3,18 @@ const year = date.getFullYear();
 
 //Get date and time
 var datetime =
-  "Last Sync: " +
-  date.getDate() +
-  "/" +
-  (date.getMonth() + 1) +
-  "/" +
-  date.getFullYear() +
-  " || " +
-  date.getHours() +
-  ":" +
-  date.getMinutes() +
-  ":" +
-  date.getSeconds();
+    "Last Sync: " +
+    date.getDate() +
+    "/" +
+    (date.getMonth() + 1) +
+    "/" +
+    date.getFullYear() +
+    " || " +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+
 console.log(datetime);
 document.getElementById("submitdate").value = datetime;
