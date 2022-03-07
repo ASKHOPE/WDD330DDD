9fd2e6239af51dee5d5661304056458894115848
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-08 02:13:02 +0530

    Finsihed

--- chamber/js/toggleviewer.js
+++ chamber/js/toggleviewer.js
@@ -9,3 +9,26 @@ xo.onclick = toggleMenu;
 
 const ox = document.getElementById("listsviewBtn");
 ox.onclick = toggleMenu;
+
+var viewtype = "O";
+var ww = window.innerWidth;
+
+if (ww >= 592 && ww <= 991) {
+  toggleMenu();
+  viewtype = "M";
+}
+
+function checkWidth() {
+  ww = window.innerWidth;
+  if (ww >= 592 && ww <= 991) {
+    if (viewtype != "M") {
+      toggleMenu();
+      viewtype = "M";
+    }
+  } else {
+    if (viewtype == "M") {
+      toggleMenu();
+      viewtype = "O";
+    }
+  }
+}
