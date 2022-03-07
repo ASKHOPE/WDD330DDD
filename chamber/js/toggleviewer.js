67981a7253983ec8762cb99c7b22422702bf9475
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-07 21:36:48 +0530

    More fixes small

--- chamber/js/toggleviewer.js
+++ chamber/js/toggleviewer.js
@@ -1,7 +1,11 @@
 function toggleMenu() {
   document.getElementById("gridviewBtn").classList.toggle("open");
-  document.getElementById("listviewBtn").classList.toggle("open");
+  document.getElementById("listsviewBtn").classList.toggle("open");
+  document.getElementById("defgridview").classList.toggle("open");
 }
 
-const xo = document.getElementById("viewerselection");
+const xo = document.getElementById("gridviewBtn");
 xo.onclick = toggleMenu;
+
+const ox = document.getElementById("listsviewBtn");
+ox.onclick = toggleMenu;
