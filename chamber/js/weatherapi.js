6ec7f31855718faee78f440e56a1e73679d45eb6
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-12 19:02:21 +0530

    Update fixed

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,34 +1,31 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Rajahmundry&units=metric&appid=76088e3b3b69023841d188f0bf640df2";
+  "https://api.openweathermap.org/data/2.5/weather?q=Rajahmundry&units=Imperial&appid=76088e3b3b69023841d188f0bf640df2";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.querySelector(".temperature").textContent = Math.round(
-      jsObject.main.temp
-    );
+    const t = jsObject.main.temp.toFixed(1);
+    document.querySelector("#current-temp").textContent = t;
+
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
+    const windsp = jsObject.wind.speed;
     document.querySelector("#weathericon").setAttribute("src", iconsrc);
     document.querySelector("#weathericon").setAttribute("alt", desc);
-    document.querySelector("figcaption").textContent = desc;
-    document.querySelector(".windspeed").textContent =
-      jsObject.wind.speed.toFixed(1);
+    document.querySelector("#desc").textContent = desc;
+    document.querySelector("#speed").textContent = windsp;
 
-    let t = jsObject.main.temp;
-    let w = jsObject.wind.speed;
-    if (t <= 10 && w > 4.83) {
+    if (t <= 50 && windsp > 3) {
       const windchill =
         35.74 +
         0.6215 * t -
-        35.75 * Math.pow(w, 0.16) +
-        0.4275 * t * Math.pow(w, 0.16);
-      document.querySelector(".windchill").innerHTML = `${Math.round(
+        35.75 * Math.pow(windsp, 0.16) +
+        0.4275 * t * Math.pow(windsp, 0.16);
+      document.querySelector("#windchill").innerHTML = `${Math.round(
         windchill
-      )}&#176;C`;
+      )}&#176;`;
     } else {
-      document.querySelector(".windchill").innerHTML = "N/A";
+      document.querySelector("#windchill").innerHTML = "N/A";
     }
   });
-
