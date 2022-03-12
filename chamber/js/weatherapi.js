bc029884e3406bbbd01563aab28d6fc348e0d29e
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-12 19:25:11 +0530

    Weather C

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,5 +1,5 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Rajahmundry&units=Imperial&appid=76088e3b3b69023841d188f0bf640df2";
+  "https://api.openweathermap.org/data/2.5/weather?q=Rajahmundry&units=metric&appid=76088e3b3b69023841d188f0bf640df2";
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -16,12 +16,12 @@ fetch(apiURL)
     document.querySelector("#desc").textContent = desc;
     document.querySelector("#speed").textContent = windsp;
 
-    if (t <= 50 && windsp > 3) {
+    if (t <= 10 && windsp > 4.8) {
       const windchill =
-        35.74 +
+        13.12 +
         0.6215 * t -
-        35.75 * Math.pow(windsp, 0.16) +
-        0.4275 * t * Math.pow(windsp, 0.16);
+        11.37 * Math.pow(windsp, 0.16) +
+        0.3965 * t * Math.pow(windsp, 0.16);
       document.querySelector("#windchill").innerHTML = `${Math.round(
         windchill
       )}&#176;`;
