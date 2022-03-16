a1fcdaf93e8f9abc663cc6a336c4599db176275d
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-17 00:48:07 +0530

    Test lv

--- chamber/js/discover_lv.js
+++ chamber/js/discover_lv.js
@@ -8,13 +8,13 @@ if (lastvisit != 0) {
 
   let daysBetween = (thisvisit - lastvisit) / FACTOR;
 
-  document.querySelector(".lastvisit").textContent =
+  document.querySelector("lastvisit").textContent =
     "It has been " +
     daysBetween.toFixed(0) +
-    " day(s) since your last visit. Welcome back!";
+    "day(s) since your last visit. Welcome back!";
 } else {
   window.localStorage.setItem("lastvisit", Date.now());
-  document.querySelector(".lastvisit").textContent =
+  document.querySelector("lastvisit").textContent =
     "This is your first visit! Find something you'll love!";
 }
 
