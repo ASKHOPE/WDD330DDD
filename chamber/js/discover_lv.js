4086b043d57c10d93ce35c214f7a196ccd64ec60
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-17 01:16:47 +0530

    Check Fix !

--- chamber/js/discover_lv.js
+++ chamber/js/discover_lv.js
@@ -1,22 +1,41 @@
 // get the stored value in localStorage
-let lastvisit = Number(window.localStorage.getItem("lastvisit"));
-
-let thisvisit = Date.now();
+let lastvisit = Number(window.localStorage.getItem('lastvisit'));
 
 if (lastvisit != 0) {
+  let thisvisit = Date.now();
+
   let FACTOR = 86400000;
 
   let daysBetween = (thisvisit - lastvisit) / FACTOR;
 
-  document.querySelector("lastvisit").textContent =
+  if (daysBetween.toFixed(0) != 1) {
+  document.querySelector('.lastvisit').textContent =
     "It has been " +
     daysBetween.toFixed(0) +
     "day(s) since your last visit. Welcome back!";
-} else {
-  window.localStorage.setItem("lastvisit", Date.now());
+
+  window.localStorage.setItem("lastvisit", thisvisit);
+
+
+/*else {
+  window.localStorage.setItem('lastvisit', Date.now());
   document.querySelector("lastvisit").textContent =
     "This is your first visit! Find something you'll love!";
+
+  window.localStorage.setItem("lastvisit", thisvisit);
+}*/
+
+
+
+} else {
+ document.querySelector('.lastvisit').textContent = 'It has been ' + daysBetween.toFixed(0) + ' day since your last visit.';
+
+    window.localStorage.setItem('lastvisit', thisvisit);
 }
 
-window.localStorage.setItem("lastvisit", thisvisit);
+} else {
 
+  window.localStorage.setItem('lastvisit', Date.now());
+  document.querySelector('.lastvisit').textContent = 'This is your first visit! Welcome!';
+
+}
