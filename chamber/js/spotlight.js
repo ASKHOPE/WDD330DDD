61ea037af1a60d183126a37bc01704b45899fa69
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-23 01:19:46 +0530

    spotlight init

---
+++ chamber/js/spotlight.js
@@ -0,0 +1,50 @@
+const requestURL = "https://askhope.github.io/wdd230/chamber/data/data.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //loop integraded in the promise
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const buziness = jsonObject["data"];
+    const bcards = document.querySelector(".bcards");
+    buziness.forEach((business) => {
+      //Injects the rest of the code here itself.//
+      let bcard = document.createElement("section");
+      let a = document.createElement("a");
+      let logo = document.createElement("img");
+      logo.setAttribute("src", business.logourl);
+      logo.setAttribute(
+        "alt",
+        `Logo of ${business.name}, located at ${business.address} and they can be reached at ${business.phone}`
+      );
+
+      let h2 = document.createElement("h2");
+      h2.textContent = `${business.name} `;
+
+      let p1 = document.createElement("p");
+      p1.textContent = ` ${business.phone}`;
+
+      let p2 = document.createElement("p");
+      p2.textContent = ` ${business.address}`;
+
+      let p3 = document.createElement("p");
+      p3.textContent = `${business.website}`;
+
+      a.href = `${business.website}`;
+      a.target = "_blank";
+      a.textContent = `${business.website}`;
+
+      //logo.setAttribute("loading", "lazy");
+      bcard.appendChild(h2);
+      bcard.appendChild(logo);
+      bcard.appendChild(p1);
+      bcard.appendChild(p2);
+      //bcard.appendChild(p3);
+      bcard.appendChild(a);
+
+      bcards.appendChild(bcard);
+      document.querySelector("div.spotlight").appendChild(bcard);
+    });
+  });
