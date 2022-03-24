d490f714edc69349933a108ae0563a92425d15ee
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-24 17:54:50 +0530

    json fix

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,56 +1,6 @@
 const spotlightData = "https://askhope.github.io/wdd230/chamber/data/data.json"; /*requestURL*/ 
-/*
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    //loop integraded in the promise
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const buziness = jsonObject["data"];
-    const bcards = document.querySelector(".spotlight-item");
-    buziness.forEach((business) => {
-      //Injects the rest of the code here itself.//
-      let bcard = document.createElement("spotlight");
-      let a = document.createElement("a");
-      let logo = document.createElement("img");
-      logo.setAttribute("src", business.logourl);
-      logo.setAttribute(
-        "alt",
-        `Logo of ${business.name}, located at ${business.address} and they can be reached at ${business.phone}`
-      );
 
-      let h2 = document.createElement("h2");
-      h2.textContent = `${business.name} `;
 
-      let p1 = document.createElement("p");
-      p1.textContent = ` ${business.phone}`;
-
-      let p2 = document.createElement("p");
-      p2.textContent = ` ${business.address}`;
-
-      let p3 = document.createElement("p");
-      p3.textContent = `${business.website}`;
-
-      a.href = `${business.website}`;
-      a.target = "_blank";
-      a.textContent = `${business.website}`;
-
-      //logo.setAttribute("loading", "lazy");
-      bcard.appendChild(h2);
-      bcard.appendChild(logo);
-      bcard.appendChild(p1);
-      bcard.appendChild(p2);
-      //bcard.appendChild(p3);
-      bcard.appendChild(a);
-
-      bcards.appendChild(bcard);
-      document.querySelector("div.spotlight").appendChild(bcard);
-    });
-  });
-*/
-
-//const spotlightData = 'https://rich20053.github.io/WDD230/chamber/data/data.json';
 var spotFilled = 0;
 
 function displaySpotlight(business, card) {  // Create elements to add to the document
