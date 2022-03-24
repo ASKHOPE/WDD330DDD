b292faa1906fe248b30d02e58dd8988e49c70eb8
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-03-24 19:52:53 +0530

    Cleaned and done

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,6 +1,5 @@
 const requestURL =
-  "https://askhope.github.io/wdd230/chamber/data/data.json"; /*spotlightData*/ 
-
+  "https://askhope.github.io/wdd230/chamber/data/data.json";  
 
 let spot1 = document.querySelector(".spotlight1");
 let spot2 = document.querySelector(".spotlight2");
@@ -23,11 +22,10 @@ fetch(requestURL)
     goldbusiness.forEach((business, index) =>
       displaySpotlight(business, index + 1)
     );
-    //   let goldbusiness = localbusiness.filter((x,index) => index < 3);
-    //   goldbusiness.forEach((business, index) => displaySpotlight(business, index+1));
+   
   });
 
-function displaySpotlight(goldbusiness, index) {
+function displaySpotlight(business, index) {
   let card = document.createElement("section");
   let h3 = document.createElement("h3");
   let logo = document.createElement("img");
@@ -39,21 +37,21 @@ function displaySpotlight(goldbusiness, index) {
 
   card.setAttribute("class", `spot${index}`);
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  logo.setAttribute("src", `${goldbusiness.logourl}`);
-  logo.setAttribute("alt", `logo for ${goldbusiness.name}`);
+  logo.setAttribute("src", `${business.logourl}`);
+  logo.setAttribute("alt", `logo for ${business.name}`);
   logo.setAttribute("class", "logo");
 
   // Change the innerHTML property of the h3 element to contain the business' info
-  h3.innerHTML = `${goldbusiness.name}`;
-  p1.textContent = `${goldbusiness.phone}`;
-  p2.textContent = `${goldbusiness.address}`;
+  h3.innerHTML = `${business.name}`;
+  p1.textContent = `${business.phone}`;
+  p2.textContent = `${business.address}`;
 
-  a.href = `${goldbusiness.website}`;
+  a.href = `${business.website}`;
   a.target = "_blank";
-  a.textContent = `${goldbusiness.website}`;
+  a.textContent = `${business.website}`;
   // Add/append the section(card) with the a element
 
-  mem.innerHTML = `${goldbusiness.membership}`;
+  mem.innerHTML = `${business.membership}`;
 
   // Add/append the section(card) with the h2 element
 
@@ -69,102 +67,4 @@ function displaySpotlight(goldbusiness, index) {
   // Add/append the existing HTML div with the cards class with the section(card)
   spot.appendChild(card);
 }
-
-
-
-
-
-/*---------OLDD--
-
-var spotFilled = 0;
-
-function displaySpotlight(business, card) {  // Create elements to add to the document
-
-    let h3 = document.createElement('h3');
-    let img1 = document.createElement('img');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    //let p4 = document.createElement('p');
-    let a = document.createElement('a');
-  
-    // Change the textContent property of the h2 element to contain the business name
-    h3.textContent = `${business.name}`;
-    h3.setAttribute('id', "bizname");
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h3);
-    // Change the src and alt properties of the img1 element to contain the business icon
-    img1.src = `${business.logourl}`;
-    img1.setAttribute('alt', `${business.name} icon`);// Set the alternate text on the img1 
-    img1.setAttribute('loading', 'lazy');
-    card.appendChild(img1);
-    // Change the textContent property of the p1 element to contain the 1 line business address
-    p1.textContent = `${business.address}`;
-    // Add/append the section(card) with the p1 element
-    card.appendChild(p1);
-    // Change the textContent property of the p2 element to contain the business phone number
-    p2.textContent = `${business.phone}`;
-    // Add/append the section(card) with the p2 element
-    card.appendChild(p2);
-    // Change the properties of the a element to contain the business website
-    a.href = `${business.website}`;
-    a.target = "_blank";
-    a.textContent = `${business.website}`;
-    // Add/append the section(card) with the a element
-    card.appendChild(a);
-    // Change the textContent property of the p3 element to contain the business contact email
-    //p3.textContent = `${business.email}`;
-    // Add/append the section(card) with the p3 element
-    // card.appendChild(p3);
-  } 
-
-function findSpotlight(business) {  // Create elements to add to the document
-    if ((business.membership != "Gold" && business.membership != "Silver") || spotFilled == 3)
-        return;
-    else {
-        if (spotFilled == 0) {
-            let bizCard = document.querySelector('#spotlight1');
-            displaySpotlight(business, bizCard);
-        }
-        if (spotFilled == 1) {
-            let bizCard = document.querySelector('#spotlight2');
-            displaySpotlight(business, bizCard);
-        }
-        if (spotFilled == 2) {
-            let bizCard = document.querySelector('#spotlight3');
-            displaySpotlight(business, bizCard);
-        }
-        spotFilled++;
-    }
-} 
-
-  getSpotlight();
-  /*getDirectory.catch(error => {
-    console.log("Error!");
-    console.error(error);
-  })
-
-  async function getSpotlight() {
-    const response = await fetch(spotlightData);
-    var bizList = await response.json();
-    const shuffledList = bizList.sort((a, b) => 0.5 - Math.random());
-/*    var bestBiz = bizList.filter( sl =>
-        sl.membership == "Gold" || 
-        sl.membership == "Silver"); 
-    shuffledList.forEach(findSpotlight);
-  }
-
-/*
-fetch(requestData)
-  .then(function (response) {
-    console.log("1st request promise")
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const business_dir = jsonObject['directory'];
-}); */
-
-
-
 
