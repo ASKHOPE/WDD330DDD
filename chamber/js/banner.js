100d99ecaa8e881b14866fd8e758da79d4d1ba8a
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-09-16 02:17:33 +0530

    REdo

--- chamber/js/banner.js
+++
@@ -1,8 +0,0 @@
-const banner_msg = document.getElementById("chamber-meeting-invite");
-const todayDate = new Date();
-const day = todayDate.getDay();
-if (day == 1 || day == 2) {
-  banner_msg.style.display = "block";
-} else {
-  banner_msg.style.display = "none";
-}
--- templeinn&suites/scripts/burger-menu.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  document.getElementById("navicron").classList.toggle("open");
-  document.getElementById("hamburburBtn").classList.toggle("open");
-}
-
-const x = document.getElementById("hamburburBtn");
-x.onclick = toggleMenu;
